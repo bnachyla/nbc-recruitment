650438cf23917f94e4e806310a0715094f597ded
bszymanowska <beata.szymanowska@bshg.com>
2025-06-02 09:46:52 +0200

    the task

---
+++ src/main/java/de/bshg/hc/bc/RecruitmentTask/home.java
@@ -0,0 +1,28 @@
+package de.bshg.hc.bc.RecruitmentTask;
+
+import de.bshg.hc.bc.RecruitmentTask.nbp.NbpRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class home {
+
+    @Autowired
+    NbpRestClient restClient;
+
+    @GetMapping(value = "/tables", produces = "application/json")
+    ResponseEntity<String> getCurrencyTables(){
+        return restClient.getCurrencyTable("a");
+    }
+
+    @GetMapping(value = "/rate", produces = "application/json")
+    ResponseEntity<String> currencyRate(
+            @RequestParam(name = "currency", defaultValue = "eur") String waluta
+    ){
+        return restClient.getCurrencyRate("a", waluta);
+    }
+
+}
